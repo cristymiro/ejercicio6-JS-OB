9d45ad15715625b2ee6f46eb74b29ffe85217312
Cris <mirocristy@gmail.com>
2022-03-22 11:26:04 +0100

    segundo commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.write('Rellenar un array con los números del 10 al 20 y mostrarlo por consola.');
+document.write('<br>')
+document.write('<br>')
+
+const myArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+document.write(myArray);
+console.log(myArray);
+
